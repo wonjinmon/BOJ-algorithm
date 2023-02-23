c402c2ce344e2fe4e1a36de4be7c76fcd76d513e
WonJin Hong <own1045@naver.com>
2023-02-23 17:28:08 +0900

    boj

---
+++ 1110.py
@@ -0,0 +1,13 @@
+# 1110 더하기 사이클
+n = int(input())
+cnt = 0
+num = n
+while True:
+    a = num // 10
+    b = num % 10
+    c = (a+b) % 10
+    num = b*10 + c
+    cnt +=1
+    if n == num:
+        break
+print(cnt)
