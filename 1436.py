127079e508611f79223757230c2725428354210e
WonJin Hong <own1045@gmail.com>
2023-06-10 14:18:49 +0900

    boj

---
+++ 1436.py
@@ -0,0 +1,12 @@
+# 1436 영화감독 숌
+N = int(input())
+cnt = 0
+end_num = 666
+
+while True:
+    if "666" in str(end_num):
+        cnt += 1
+    if cnt == N:
+        print(end_num)
+        break
+    end_num += 1
